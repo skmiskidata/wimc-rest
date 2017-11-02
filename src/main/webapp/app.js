10a25f9fd15dbed32a5d6df0ce9aa813ea279bbc
Harald <harald.mue@gmail.com>
2017-11-02 16:50:46 +0100

    Some UI-related stuff - mainly for testing.

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -46,3 +46,46 @@ $(function () {
     $( "#disconnect" ).click(function() { disconnect(); });
     $( "#send" ).click(function() { sendName(); });
 });
+
+function initVehicle(id, licenseplate) {
+    var defer = $.Deferred();
+    var container = Snap("#carpark");
+    Snap.load("asset/plate.svg", function(lp) {
+
+        var rootelem = lp.select('#licenseplate');
+        rootelem.attr({ id: 'lp_'+id, viewBox: '0 0 3840 2160' });
+
+        var lpelem = lp.select('#licenseplate_number');
+        lpelem.node.innerHTML = licenseplate;
+
+        var mainlayer = lp.select('#main_layer');
+        container.append(lp);
+
+        mainlayer.hover(function() {
+                var localMatrix = mainlayer.transform().localMatrix;
+                if (localMatrix.a === 1 && localMatrix.d === 1) {
+                    var transform = new Snap.Matrix();
+                    transform.add(localMatrix);
+                    transform.scale(1.5);
+                    mainlayer.stop().animate({transform: transform}, 200);
+                }
+            },
+            function() {
+                var localMatrix = mainlayer.transform().localMatrix;
+                if (localMatrix.a == 1.5 && localMatrix.d == 1.5) {
+                    var transform = new Snap.Matrix();
+                    transform.add(localMatrix);
+                    transform.scale();
+                    mainlayer.stop().animate({transform: transform}, 200);
+                }
+            });
+        defer.resolve(lp);
+    });
+    return defer.promise();
+}
+
+function moveVehicle(id, dx, dy) {
+    var lp = Snap.select("#lp_"+id);
+    var mainlayer = lp.select('#main_layer');
+    mainlayer.stop().animate({ transform: 'T'+dx+','+dy }, 200);
+}
