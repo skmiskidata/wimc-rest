19140133eda9c8df8a45c5fc35ba3dc498e7e497
Harald <harald.mue@gmail.com>
2017-11-08 13:23:01 +0100

    Some UI-related stuff - mainly for testing.

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,17 +1,5 @@
 var stompClient = null;
 
-function setConnected(connected) {
-    $("#connect").prop("disabled", connected);
-    $("#disconnect").prop("disabled", !connected);
-    if (connected) {
-        $("#conversation").show();
-    }
-    else {
-        $("#conversation").hide();
-    }
-    $("#lp-out").html("");
-}
-
 function connect() {
     var socket = new SockJS('/gs-guide-websocket');
     stompClient = Stomp.over(socket);
@@ -20,7 +8,7 @@ function connect() {
         console.log('Connected: ' + frame);
         stompClient.subscribe('/topic/lp', function (lpmessage) {
             console.log(lpmessage.body);
-            showPlate(JSON.parse(lpmessage.body));
+            //showPlate(JSON.parse(lpmessage.body));
         });
     });
 }
@@ -32,20 +20,6 @@ function disconnect() {
     setConnected(false);
     console.log("Disconnected");
 }
-
-function showPlate(lpmessage) {
-    console.log(lpmessage);
-    $("#lp-out").append("<tr><td>" + lpmessage.licencePlate + "</td><td>" + lpmessage.plateCoordinates[0].x + "</td><td>" + lpmessage.plateCoordinates[0].y + "</td><td>" + lpmessage.plateCoordinates[1].x + "</td><td>" + lpmessage.plateCoordinates[1].y + "</td><td>" + lpmessage.plateCoordinates[2].x + "</td><td>" + lpmessage.plateCoordinates[2].y + "</td><td>" + lpmessage.plateCoordinates[3].x + "</td><td>" + lpmessage.plateCoordinates[3].y + "</td><td>" + lpmessage.uuid + "</td><td>" + lpmessage.confidence + "</td><td>" + lpmessage.draw + "</td></tr>");
-}
-
-$(function () {
-    $("form").on('submit', function (e) {
-        e.preventDefault();
-    });
-    $( "#connect" ).click(function() { connect(); });
-    $( "#disconnect" ).click(function() { disconnect(); });
-    $( "#send" ).click(function() { sendName(); });
-});
 
 function initVehicle(id, licenseplate) {
     var defer = $.Deferred();
